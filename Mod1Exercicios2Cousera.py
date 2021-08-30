cc40a3929ec355ad23f4ca460ea9dfd305a5fb68
WsaWudang <wesleyspf@hotmail.com>
2021-08-30 19:37:50 -0300

    Adicionar arquivos de projeto.

---
+++ Mod1Exercicios2Cousera.py
@@ -0,0 +1,10 @@
+nota_1 = int(input("Digite a primeira nota: "))
+nota_2 = int(input("Digite a segunda nota: "))
+nota_3 = int(input("Digite a terceira nota: "))
+nota_4 = int(input("Digite a quarta nota: "))
+
+soma = nota_1 + nota_2 + nota_3 + nota_4
+
+media = soma / 4
+
+print("A média aritimédica é",media)
